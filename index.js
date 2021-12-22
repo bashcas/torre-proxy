058f2cead8dbac7262fefc7ca26af7a7a68752cc
Sebastián Castañeda <sebastiansnipez@gmail.com>
2021-12-22 15:48:02 -0500

    Search endpoint proxied

--- index.js
+++ index.js
@@ -1,6 +1,11 @@
 const express = require("express")
 const request = require("request")
+const cors = require("cors")
 const app = express()
+
+app.use(express.json())
+
+app.use(cors())
 
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
@@ -10,6 +15,25 @@ app.use((req, res, next) => {
 app.get("/", (req, res) => {
   const url = req.query.url
   request({ url: url }, (err, response, body) => {
+    if (err) {
+      console.log(err)
+      res.send(err)
+    } else {
+      res.json(JSON.parse(body))
+    }
+  })
+})
+
+app.post("/", (req, res) => {
+  const url = req.query.url
+  const body = JSON.stringify(req.body)
+  const options = {
+    url: url,
+    method: "POST",
+    body: body,
+    headers: { "Content-Type": "application/json" },
+  }
+  request(options, (err, response, body) => {
     if (err) {
       console.log(err)
       res.send(err)
